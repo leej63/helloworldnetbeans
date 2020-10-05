aedac54d4d6057b92756010405da98ad99da4fc6
Jonathan Lee <jonathan_496@hotmail.com>
2020-10-05 14:33:03 -0700

    Hello World in NetBeans

---
+++ src/main/java/com/jonathanlee/helloworld/Hello.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Jonathan Lee
+ * email: jonathan_496@hotmail.com
+ * date: October 5, 2020
+ * purpose: My first Hello World program is an IDE
+ */
+package com.jonathanlee.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jonathan!");
+    }
+}
